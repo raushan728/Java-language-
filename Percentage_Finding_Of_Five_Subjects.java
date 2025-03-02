cd81ad307503df67a2bf2e0c90c77b83d56d6d37
raushan728 <raushansinghrajpoot687@gmail.com>
2025-03-02 19:58:35 +0530

    Add files via upload

---
+++ Percentage_Finding_Of_Five_Subjects.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class finding_percentage {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter your Hindi marks");
+        int a = s.nextInt();
+        System.out.println("enter your English marks");
+        int b = s.nextInt();
+        System.out.println("enter your chemistry marks");
+        int c = s.nextInt();
+        System.out.println("enter your physics marks");
+        int d = s.nextInt();
+        System.out.println("enter your math marks");
+        int e = s.nextInt();
+        int total = a + b + c + d + e;
+        float p = (total / 500f) * 100;
+        System.out.println("percentage is\n" +p);
+        s.close();
+    }
+}
