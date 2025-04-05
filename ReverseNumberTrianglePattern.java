8753871141fae89064c148659e3199be11386f2a
RAUSHAN KUMAR <raushansinghrajpoot687@gmail.com>
2025-04-05 15:29:24 +0530

    Add files via upload

---
+++ ReverseNumberTrianglePattern.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ReverseNumberTrianglePattern {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number :");
+        int n = s.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+
+            }
+            System.out.println();
+
+        }
+        s.close();
+    }
+}
